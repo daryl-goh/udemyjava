1ce2482c5554e82dc5d4e02573ffc0570c26db67
daryl-goh <goh.daryl@gmail.com>
2022-07-22 19:41:06 +0800

    edited SortedArray.java

--- sortedarray/SortedArray.java
+++ sortedarray/SortedArray.java
@@ -1,46 +1,48 @@
 package sortedarray;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
+ 
 public class SortedArray {
-    
-    public static int[] getIntegers(int capacity) {
-        Scanner scanner = new Scanner(System.in);
-        int[] intArray = new int[capacity];
-     
-        for (int i = 0; i < intArray.length; i++) {
-          System.out.printf("Enter %d integer value: ", i);
-          intArray[i] = scanner.nextInt();
-        }
-     
-        scanner.close();
-        return intArray;
+ 
+  public static int[] getIntegers(int capacity) {
+    Scanner scanner = new Scanner(System.in);
+    int[] array = new int[capacity];
+ 
+    for (int i = 0; i < array.length; i++) {
+      System.out.printf("Enter %d integer value: ", i);
+      array[i] = scanner.nextInt();
     }
-    
-    public static void printArray(int[] intArray) {
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println("Element " + i + " contents " + intArray[i]);
-        }
+ 
+    scanner.close();
+ 
+    return array;
+  }
+ 
+  public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.printf("Element %d contents %d\n", i, array[i]);
     }
-    
-    public static int[] sortIntegers (int[] intArray) {
-        int[] sortedArray = new int[intArray.length];
-        for (int i = 0; i < intArray.length; i++) {
-            sortedArray[i] = intArray[i];
+  }
+ 
+  public static int[] sortIntegers(int[] array) {
+    int[] sortedArray = Arrays.copyOf(array, array.length);
+ 
+    boolean flag = true;
+    int tmp;
+    while (flag) {
+      flag = false;
+      for (int i = 0; i < sortedArray.length - 1; i++) {
+        if (sortedArray[i] < sortedArray[i + 1]) {
+          tmp = sortedArray[i];
+          sortedArray[i] = sortedArray[i + 1];
+          sortedArray[i + 1] = tmp;
+          flag = true;
         }
-        boolean flag = true;
-        int temp;
-        while(flag) {
-            flag = false;
-            for (int i = 0; i < sortedArray.length-1; i++) {
-                if(sortedArray[i] < sortedArray[i+1]) {
-                    temp = sortedArray[i];
-                    sortedArray[i] = sortedArray[i+1];
-                    sortedArray[i] = temp;
-                    flag = true;
-                }
-            }
-        }
-        return sortedArray;
-    }   
+      }
+    }
+ 
+    return sortedArray;
+  }
 }
